373582839bae323ac2b3eb2b0599e6f7479e5610
Charles Pelton <charliejp0311@gmail.com>
2020-03-24 16:03:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       VALUES (?, ?);
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    id_querry = <<-SQL SELECT id FROM students WHERE name = ?;SQL
+    id_querry = <<-SQL SELECT id FROM students WHERE name = (?);SQL
     ans = DB[:conn].execute(id_querry, self.name)
     # binding.pry
   end
